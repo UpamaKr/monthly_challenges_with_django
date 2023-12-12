25b31c06e706d8c44dd58b9a128a9b3c7c63f3e5
UpamaKr <upama.sarker@yahoo.com>
2023-12-12 21:04:45 +0100

    2nd change

--- challenges/views.py
+++ challenges/views.py
@@ -28,6 +28,21 @@ monthly_challenges = {
     "december": "Review and refactor an old project"
 }
 
+def index(request):
+    list_items = ''
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse('month-challenge', args =[month])
+
+        list_items += f"<li><a href = \"{month_path}\">{capitalized_month}</a></li>"
+       
+
+    response_data = f'<ul>{list_items}</ul>'
+    return HttpResponse(response_data)
+
+
 def monthly_challenge_by_number(request, month):
     months =list(monthly_challenges.keys())
     if month> len(months):
@@ -42,7 +57,8 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f'<h1>{challenge_text}</h1>'
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("This month is not supported!")
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
     
